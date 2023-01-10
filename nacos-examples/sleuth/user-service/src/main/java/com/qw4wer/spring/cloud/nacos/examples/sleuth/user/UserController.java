e07c22c7db2504277fc7465542b31964e9c02084
qw4wer <qw5wer@gmail.com>
2023-01-10 16:17:14 +0800

    sleuth+zipkin 测试

---
+++ nacos-examples/sleuth/user-service/src/main/java/com/qw4wer/spring/cloud/nacos/examples/sleuth/user/UserController.java
@@ -0,0 +1,15 @@
+package com.qw4wer.spring.cloud.nacos.examples.sleuth.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "hello";
+    }
+}
